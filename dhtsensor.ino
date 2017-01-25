c48b54fa66e4c0d495e5193a46497a4e3ba1d7f3
Norman <norman-thomas@users.noreply.github.com>
2017-01-25 21:14:37 +0100

    Reduce number of events
    - reduced number of events triggers, Particle.io limits the rate to one per second and a burst of 4 events in one second at most
    - measure dewPointSlow

--- dhtsensor.ino
+++ dhtsensor.ino
@@ -1,4 +1,5 @@
 #include <PietteTech_DHT.h>
+#include <math.h>
 
 #define DHTPIN D3
 #define DHTTYPE DHT22
@@ -9,6 +10,7 @@ PietteTech_DHT DHT(DHTPIN, DHTTYPE, dht_wrapper);
 double humidity = 0.0;
 double temperature = 0.0;
 double dewPoint = 0.0;
+double dewPointSlow = 0.0;
 int count = 0;
 String myName = "/";
 
@@ -31,11 +33,10 @@ void setup() {
 }
 
 float roundReading(float reading) {
-    return ((int) (reading * 10)) / 10.0;
+    return (floor(reading * 100)) / 100.0;
 }
 
 void loop() {
-    
     if (count == 0) {
         int result = 0;
         while ((result = DHT.acquireAndWait()) != DHTLIB_OK) {
@@ -46,23 +47,19 @@ void loop() {
         humidity = roundReading( DHT.getHumidity() );
         temperature = roundReading( DHT.getCelsius() );
         dewPoint = roundReading( DHT.getDewPoint() );
+        dewPointSlow = roundReading( DHT.getDewPointSlow() );
     }
     
     Serial.println(temperature, 2);
     Serial.println(humidity, 2);
+    Serial.println(dewPoint, 2);
+    
+    Particle.publish(myName + "temperature", String(temperature), 60, PRIVATE);
+    Particle.publish(myName + "humidity", String(humidity), 60, PRIVATE);
+    
+    String json_condition = "{ \"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + ", \"dewPoint\":" + String(dewPoint) + ", \"dewPointSlow\":" + String(dewPointSlow) + " }";
+    Particle.publish(myName + "json_condition", json_condition, 60, PRIVATE);
 
-    Particle.publish("temperature", String(temperature), PRIVATE);
-    Particle.publish("humidity", String(humidity), PRIVATE);
-    
-    Particle.publish(myName + "temperature", String(temperature), PRIVATE);
-    Particle.publish(myName + "humidity", String(humidity), PRIVATE);
-    
-    String condition = "temperature=" + String(temperature) + ",humidity=" + String(humidity);
-    String json_condition = "{ \"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + " }";
-    
-    Particle.publish("condition", condition, PRIVATE);
-    Particle.publish("json_condition", json_condition, PRIVATE);
-    
     count = (count + 1) % 4;
 
     delay(30000);
