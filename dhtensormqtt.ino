6f70f8785a253a19e7b83efc8cec606b34818477
Norman <norman-thomas@users.noreply.github.com>
2017-06-25 14:23:56 +0200

    Cleanup
    - reduce particle events to just one (`json_condition`)
    - remove unused sample code from MQTT

--- dhtensormqtt.ino
+++ dhtensormqtt.ino
@@ -14,8 +14,7 @@ double temperature = 0.0;
 double dewPoint = 0.0;
 double dewPointSlow = 0.0;
 int count = 0;
-String myName = "/";
-String myPureName = "";
+String myName = "";
 
 
 void callback(char* topic, byte* payload, unsigned int length);
@@ -86,26 +85,6 @@ void setup() {
 
     // add qos callback. If don't add qoscallback, ACK message from MQTT server is ignored.
     client.addQosCallback(qoscallback);
-
-    // publish/subscribe
-    if (client.isConnected()) {
-        // it can use messageid parameter at 4.
-        uint16_t messageid;
-        client.publish("outTopic/message", "hello world QOS1", MQTT::QOS1, &messageid);
-        Serial.println(messageid);
-
-        // if 4th parameter don't set or NULL, application can not check the message id to the ACK message from MQTT server. 
-        client.publish("outTopic/message", "hello world QOS1(message is NULL)", MQTT::QOS1);
-        
-        // QOS=2
-        client.publish("outTopic/message", "hello world QOS2", MQTT::QOS2, &messageid);
-        Serial.println(messageid);
-
-        // save QoS2 message id as global parameter.
-        qos2messageid = messageid;
-
-        client.subscribe("inTopic/message");
-    }
 }
 
 float roundReading(float reading) {
@@ -124,22 +103,22 @@ void loop() {
         temperature = roundReading( DHT.getCelsius() );
         dewPoint = roundReading( DHT.getDewPoint() );
         dewPointSlow = roundReading( DHT.getDewPointSlow() );
-    
-        client.publish(myPureName + "/temperature", String(temperature));
-        client.publish(myPureName + "/humidity", String(humidity));
-        client.publish(myPureName + "/dewPoint", String(dewPoint));
-        client.publish(myPureName + "/dewPointSlow", String(dewPointSlow));
     }
     
     Serial.println(temperature, 2);
     Serial.println(humidity, 2);
     Serial.println(dewPoint, 2);
     
-    //Particle.publish(myName + "temperature", String(temperature), 60, PRIVATE);
-    //Particle.publish(myName + "humidity", String(humidity), 60, PRIVATE);
-    
-    String json_condition = "{ \"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + ", \"dewPoint\":" + String(dewPoint) + ", \"dewPointSlow\":" + String(dewPointSlow) + " }";
-    Particle.publish(myName + "json_condition", json_condition, 60, PRIVATE);
+    if (myName.length() > 0)
+    {
+        client.publish(myName + "/temperature", String(temperature));
+        client.publish(myName + "/humidity", String(humidity));
+        client.publish(myName + "/dewPoint", String(dewPoint));
+        client.publish(myName + "/dewPointSlow", String(dewPointSlow));
+            
+        String json_condition = "{ \"temperature\": " + String(temperature) + ", \"humidity\": " + String(humidity) + ", \"dewPoint\":" + String(dewPoint) + ", \"dewPointSlow\":" + String(dewPointSlow) + " }";
+        Particle.publish("/" + myName + "/json_condition", json_condition, 60, PRIVATE);
+    }
     
     count = (count + 1) % 2;
     
@@ -150,7 +129,6 @@ void loop() {
 }
 
 void nameHandler(const char *topic, const char *data) {
-    myPureName = String(data);
-    myName = "/" + myPureName + "/";
-    Particle.publish("my name is: " + String(topic), myName);
+    myName = String(data);
+    Particle.publish("my name is:", myName);
 }
